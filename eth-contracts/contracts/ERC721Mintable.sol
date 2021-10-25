cdaca4667a7f1e824063a5a180355e0986aa91e1
Jeromy Cannon <JeromyLewisCannon@gmail.com>
2021-10-25 13:38:15 -0500

    completed some Ownable todos

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -7,15 +7,39 @@ import "openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol";
 import "./Oraclize.sol";
 
 contract Ownable {
-    //  TODO: 1) create a private '_owner' variable of type address with a public getter function
-    //  TODO: 2) create an internal constructor that sets the _owner var to the creater of the contract
-    //  TODO: 3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
-    //  TODO: 4) fill out the transferOwnership function
-    //  TODO: 5) create an event that emits anytime ownerShip is transfered (including in the constructor)
+    //  DONE: 1) create a private '_owner' variable of type address with a public getter function
+    address private _owner;
+
+    event OwnershipTransfer(address oldOwner, address newOwner);
+
+    function owner() public returns (address) {
+        return _owner;
+    }
+
+    //  DONE: 2) create an internal constructor that sets the _owner var to the creater of the contract
+    constructor() public {
+        emit OwnershipTransfer(_owner, msg.sender);
+        _owner = msg.sender;
+    }
+
+    //  DONE: 3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
+    modifier onlyOwner() {
+        require(
+            msg.sender == _owner,
+            "this function can only be called by the owner of the contract"
+        );
+        _;
+    }
+
+    //  DONE: 4) fill out the transferOwnership function
+    //  DONE: 5) create an event that emits anytime ownerShip is transfered (including in the constructor)
 
     function transferOwnership(address newOwner) public onlyOwner {
-        // TODO:  add functionality to transfer control of the contract to a newOwner.
-        // TODO: make sure the new owner is a real address
+        // DONE: make sure the new owner is a real address
+        require(newOwner != address(0), "new owner cannot be 0x0 address");
+        // DONE: add functionality to transfer control of the contract to a newOwner.
+        emit OwnershipTransfer(_owner, newOwner);
+        _owner = newOwner;
     }
 }
 
